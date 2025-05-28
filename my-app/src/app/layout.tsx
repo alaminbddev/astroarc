90b99e1924860fdccd385ac331c639b5c064a6da
alaminbddev <alamin.webuinet@gmail.com>
2025-05-28 23:12:56 +0600

    Update layout.tsx

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -2,10 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./components/spacing.css";
-import "./components/utils.css";
-import "./components/reset.css";
 import "./components/typography.css";
-import "./components/components.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
